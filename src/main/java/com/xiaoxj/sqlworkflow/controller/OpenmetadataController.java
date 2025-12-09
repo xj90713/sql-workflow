74e5649ef9703be60bef2019c3bce0a7bc0d89ed
xiaoxiaojun <xiaoxiaojun@gushi.com>
2025-12-09 10:36:46 +0800

    add ci file[ci skip]

---
+++ src/main/java/com/xiaoxj/sqlworkflow/controller/OpenmetadataController.java
@@ -0,0 +1,40 @@
+package com.xiaoxj.sqlworkflow.controller;
+
+import com.xiaoxj.sqlworkflow.exception.GlobalExceptionHandler;
+import com.xiaoxj.sqlworkflow.service.OpenmetadataService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Map;
+
+@RestController
+public class OpenmetadataController {
+
+    private static final Logger log = LoggerFactory.getLogger(OpenmetadataController.class);
+
+    @Autowired
+    private OpenmetadataService openmetadataService;
+
+    @PostMapping("/api/openmetadata/lineage")
+    public Map<String, Object> getSqlLineage(@RequestBody Map<String, String> payload) {
+        String filePath = payload.get("file_path");
+        String fileName = filePath.substring(filePath.lastIndexOf("/") + 1);
+        String content = payload.get("content");
+        byte[] decodedBytes = Base64.getDecoder().decode(content);
+        String sqlContent = new String(decodedBytes, StandardCharsets.UTF_8);
+        sqlContent = sqlContent.replace("${pt_day}", "'2025-01-01'")
+                .replace("${imp_pt_date}", "'2025-01-01'");
+        long start = System.currentTimeMillis();
+        String resp = openmetadataService.getSqlLineage(fileName, sqlContent);
+        long cost = System.currentTimeMillis() - start;
+        log.info("file_name={}, duration_ms={}", fileName, cost);
+        return java.util.Map.of("duration_ms", cost, "file_name", fileName, "data", resp);
+    }
+}
